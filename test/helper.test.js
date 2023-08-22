370d2176887daa28a3d3512880fc30669efe1dfd
Naveen Kandula <naveen.kandula@edfora.com>
2023-08-22 19:47:28 +0530

    latest changes

---
+++ test/helper.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe.only('Notification Manager', () => {
+    beforeEach(() => {
+        console.log("hello");
+    });
+    it('Notification sucess case ', async () => {
+    
+    });
+});
