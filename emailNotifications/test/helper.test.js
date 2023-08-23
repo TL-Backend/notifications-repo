029c34f999e98360fb80affca80f22572d3bc9a0
Naveen Kandula <naveen.kandula@edfora.com>
2023-08-23 11:48:13 +0530

    Total test

--- emailNotifications/test/helper.test.js
+++ emailNotifications/test/helper.test.js
@@ -1,142 +1,140 @@
-// const chai = require("chai"); // Replace '../index' with the correct path to your index.js file
+const chai = require("chai"); // Replace '../index' with the correct path to your index.js file
 
-// const rewire = require("rewire");
-// const helperMain = rewire("../helper.js");
+const rewire = require("rewire");
+const helperMain = rewire("../helper.js");
 
-// const expect = chai.expect;
+const expect = chai.expect;
 
-// const sendEMAILNotificationMock = () => {
-//   return { code: 200, error: false };
-// };
+const sendEMAILNotificationMock = () => {
+  return { code: 200, error: false };
+};
 
-// const updateDBNotificationMock = () => {
-//   return { code: 200, error: false };
-// };
+const updateDBNotificationMock = () => {
+  return { code: 200, error: false };
+};
 
-// const sendEMAILNotificationMockWithExeception = () => {
-//   return { code: 200, error: true };
-// };
+const sendEMAILNotificationMockWithExeception = () => {
+  return { code: 200, error: true };
+};
 
-// const updateDBNotificationMockWithExeception = () => {
-//   return { code: 200, error: true };
-// };
+const updateDBNotificationMockWithExeception = () => {
+  return { code: 200, error: true };
+};
 
-// helperMain.__set__("sendEMAILNotification", sendEMAILNotificationMock);
-// helperMain.__set__("updateDBNotification", updateDBNotificationMock);
+helperMain.__set__("sendEMAILNotification", sendEMAILNotificationMock);
+helperMain.__set__("updateDBNotification", updateDBNotificationMock);
 
-// describe("Email Notification Sender", () => {
-//   it("Successfully send email notification", async () => {
-//     const resp = await helperMain.emailNotificationHelper({
-//       email: "vikas@tilicho.in",
-//       subject: "hello",
-//       body: "World",
-//       notification_id: 22,
-//     });
-//     expect(resp.error).to.equal(false);
-//   });
+describe("Email Notification Sender", () => {
+  it("Successfully send email notification", async () => {
+    const resp = await helperMain.emailNotificationHelper({
+      email: "vikas@tilicho.in",
+      subject: "hello",
+      body: "World",
+      notification_id: 22,
+    });
+    expect(resp.error).to.equal(false);
+  });
 
-//   it("Failed send email notification due to DB faiure", async () => {
-//     helperMain.__set__(
-//       "updateDBNotification",
-//       updateDBNotificationMockWithExeception,
-//     );
-//     const resp = await helperMain.emailNotificationHelper({
-//       email: "vikas@tilicho.in",
-//       subject: "hello",
-//       body: "World",
-//       notification_id: 22,
-//     });
-//     expect(resp.error).to.equal(true);
-//     expect(resp.message).to.equal("Internal error");
-//   });
+  it("Failed send email notification due to DB faiure", async () => {
+    helperMain.__set__(
+      "updateDBNotification",
+      updateDBNotificationMockWithExeception,
+    );
+    const resp = await helperMain.emailNotificationHelper({
+      email: "vikas@tilicho.in",
+      subject: "hello",
+      body: "World",
+      notification_id: 22,
+    });
+    expect(resp.error).to.equal(true);
+    expect(resp.message).to.equal("Internal error");
+  });
 
-//   it("Failed send email notification due to Email sender", async () => {
-//     helperMain.__set__("updateDBNotification", updateDBNotificationMock);
-//     helperMain.__set__(
-//       "sendEMAILNotification",
-//       sendEMAILNotificationMockWithExeception,
-//     );
-//     const resp = await helperMain.emailNotificationHelper({
-//       email: "vikas@tilicho.in",
-//       subject: "hello",
-//       body: "World",
-//       notification_id: 22,
-//     });
-//     expect(resp.error).to.equal(true);
-//     expect(resp.message).to.equal("Internal error");
-//   });
+  it("Failed send email notification due to Email sender", async () => {
+    helperMain.__set__("updateDBNotification", updateDBNotificationMock);
+    helperMain.__set__(
+      "sendEMAILNotification",
+      sendEMAILNotificationMockWithExeception,
+    );
+    const resp = await helperMain.emailNotificationHelper({
+      email: "vikas@tilicho.in",
+      subject: "hello",
+      body: "World",
+      notification_id: 22,
+    });
+    expect(resp.error).to.equal(true);
+    expect(resp.message).to.equal("Internal error");
+  });
 
-//   it("Failed send email notification due to Invalid input", async () => {
-//     helperMain.__set__("updateDBNotification", updateDBNotificationMock);
-//     helperMain.__set__(
-//       "sendEMAILNotification",
-//       sendEMAILNotificationMockWithExeception,
-//     );
-//     const resp = await helperMain.emailNotificationHelper({
-//       subject: "hello",
-//       body: "World",
-//       notification_id: 22,
-//     });
-//     expect(resp.error).to.equal(true);
-//     expect(resp.message).to.equal("Internal error");
-//   });
-// });
-
-// helperMain.__set__("notification_audits.update", async () => {
-//   return {
-//     id: 1233,
-//   };
-// });
-// describe("Update Notification test suit ", () => {
-//   it("Successfully updated notification with success response", async () => {
-//     const resp = await helperMain.updateDBNotification(
-//       { error: false, message: "fhvlf" },
-//       79,
-//     );
-//     expect(resp.error).to.equal(false);
-//   });
-
-//   it("Successfully updated notification with failure response", async () => {
-//     const resp = await helperMain.updateDBNotification(
-//       { error: true, message: "fhvlf" },
-//       79,
-//     );
-//     expect(resp.error).to.equal(false);
-//   });
-
-//   it("Invalid params error", async () => {
-//     const resp = await helperMain.updateDBNotification();
-//     expect(resp.error).to.equal(true);
-//   });
-//   it("notification audit update failed case", async () => {
-//     helperMain.__set__("notification_audits.update", async () => {
-//       throw new Error("updation failed");
-//     });
-//     const resp = await helperMain.updateDBNotification(
-//       { err: false, message: "fhvlf" },
-//       79,
-//     );
-//     expect(resp.error).to.equal(true);
-//   });
-// });
-
-// describe("Send SMS test suit ", () => {
-//   it("Successfully send sms notification with sucess response", async () => {
-//     helperMain.__set__("sendEmail", async () => {
-//       return {
-//         error: false,
-//         message: { messageId: "653bdbbdd" },
-//       };
-//     });
-//     const resp = await helperMain.sendEMAILNotification({
-//       email: "vikas@tilicho.in",
-//       subject: "hello",
-//       body: "World",
-//     });
-//     expect(resp.error).to.equal(false);
-//   });
-// });
-test('1 + 1 = 2', () => {
-  expect(1 + 1).toBe(2);
+  it("Failed send email notification due to Invalid input", async () => {
+    helperMain.__set__("updateDBNotification", updateDBNotificationMock);
+    helperMain.__set__(
+      "sendEMAILNotification",
+      sendEMAILNotificationMockWithExeception,
+    );
+    const resp = await helperMain.emailNotificationHelper({
+      subject: "hello",
+      body: "World",
+      notification_id: 22,
+    });
+    expect(resp.error).to.equal(true);
+    expect(resp.message).to.equal("Internal error");
+  });
 });
+
+helperMain.__set__("notification_audits.update", async () => {
+  return {
+    id: 1233,
+  };
+});
+describe("Update Notification test suit ", () => {
+  it("Successfully updated notification with success response", async () => {
+    const resp = await helperMain.updateDBNotification(
+      { error: false, message: "fhvlf" },
+      79,
+    );
+    expect(resp.error).to.equal(false);
+  });
+
+  it("Successfully updated notification with failure response", async () => {
+    const resp = await helperMain.updateDBNotification(
+      { error: true, message: "fhvlf" },
+      79,
+    );
+    expect(resp.error).to.equal(false);
+  });
+
+  it("Invalid params error", async () => {
+    const resp = await helperMain.updateDBNotification();
+    expect(resp.error).to.equal(true);
+  });
+  it("notification audit update failed case", async () => {
+    helperMain.__set__("notification_audits.update", async () => {
+      throw new Error("updation failed");
+    });
+    const resp = await helperMain.updateDBNotification(
+      { err: false, message: "fhvlf" },
+      79,
+    );
+    expect(resp.error).to.equal(true);
+  });
+});
+
+describe("Send SMS test suit ", () => {
+  it("Successfully send sms notification with sucess response", async () => {
+    helperMain.__set__("sendEmail", async () => {
+      return {
+        error: false,
+        message: { messageId: "653bdbbdd" },
+      };
+    });
+    const resp = await helperMain.sendEMAILNotification({
+      email: "vikas@tilicho.in",
+      subject: "hello",
+      body: "World",
+    });
+    expect(resp.error).to.equal(false);
+  });
+});
+
 
--- smsNotifications/test/helper.test.js
+++ smsNotifications/test/helper.test.js
@@ -1,154 +1,151 @@
-// const chai = require("chai"); // Replace '../index' with the correct path to your index.js file
+const chai = require("chai"); // Replace '../index' with the correct path to your index.js file
 
-// const rewire = require("rewire");
-// const helperMain = rewire("../helper.js");
+const rewire = require("rewire");
+const helperMain = rewire("../helper.js");
 
-// const expect = chai.expect;
+const expect = chai.expect;
 
-// const sendSMSNotificationMock = () => {
-//   return { code: 200, error: false };
-// };
+const sendSMSNotificationMock = () => {
+  return { code: 200, error: false };
+};
 
-// const updateDBNotificationMock = () => {
-//   return { code: 200, error: false };
-// };
+const updateDBNotificationMock = () => {
+  return { code: 200, error: false };
+};
 
-// const sendSMSNotificationMockWithException = () => {
-//     return {
-//         error: true
-//     }
-// }
+const sendSMSNotificationMockWithException = () => {
+    return {
+        error: true
+    }
+}
 
-// const updateDBNotificationMockWithException = () => {
-//     return {
-//         error: true
-//     }
-// }
+const updateDBNotificationMockWithException = () => {
+    return {
+        error: true
+    }
+}
 
-// helperMain.__set__("sendSMSNotification", sendSMSNotificationMock);
-// helperMain.__set__("updateDBNotification", updateDBNotificationMock);
+helperMain.__set__("sendSMSNotification", sendSMSNotificationMock);
+helperMain.__set__("updateDBNotification", updateDBNotificationMock);
 
-// describe("SMS Notification Sender", () => {
-//   it("Successful SMS notification", async () => {
-//     const resp = await helperMain.smsNotificationHelper({
-//       message_content: "kvnhdlvs",
-//       mobile: "+91838385415",
-//       notification_id: 100,
-//     });
-//     expect(resp.error).to.equal(false);
-//     expect(resp.message).to.equal("SMS successfully sent!");
-//   });
+describe("SMS Notification Sender", () => {
+  it("Successful SMS notification", async () => {
+    const resp = await helperMain.smsNotificationHelper({
+      message_content: "kvnhdlvs",
+      mobile: "+91838385415",
+      notification_id: 100,
+    });
+    expect(resp.error).to.equal(false);
+    expect(resp.message).to.equal("SMS successfully sent!");
+  });
 
-//   it("Invalid input case", async () => {
-//     const resp = await helperMain.smsNotificationHelper({
-//       notification_id: 100,
-//     });
-//     expect(resp.error).to.equal(true);
-//     expect(resp.message).to.equal("Internal error");
-//   });
+  it("Invalid input case", async () => {
+    const resp = await helperMain.smsNotificationHelper({
+      notification_id: 100,
+    });
+    expect(resp.error).to.equal(true);
+    expect(resp.message).to.equal("Internal error");
+  });
 
-//   it("Failed SMS notification due to sender", async () => {
-//     helperMain.__set__("sendSMSNotification", sendSMSNotificationMockWithException);
-//     const resp = await helperMain.smsNotificationHelper({
-//       message_content: "kvnhdlvs",
-//       mobile: "+91838385415",
-//       notification_id: 100,
-//     });
-//     expect(resp.error).to.equal(true);
-//     expect(resp.message).to.equal("Internal error");
-//   });
+  it("Failed SMS notification due to sender", async () => {
+    helperMain.__set__("sendSMSNotification", sendSMSNotificationMockWithException);
+    const resp = await helperMain.smsNotificationHelper({
+      message_content: "kvnhdlvs",
+      mobile: "+91838385415",
+      notification_id: 100,
+    });
+    expect(resp.error).to.equal(true);
+    expect(resp.message).to.equal("Internal error");
+  });
 
-//   it("Failed SMS notification due to DB save", async () => {
-//     helperMain.__set__("sendSMSNotification", sendSMSNotificationMock);
-//     helperMain.__set__("updateDBNotification", updateDBNotificationMockWithException);
-//     const resp = await helperMain.smsNotificationHelper({
-//       message_content: "kvnhdlvs",
-//       mobile: "+91838385415",
-//       notification_id: 100,
-//     });
-//     expect(resp.error).to.equal(true);
-//     expect(resp.message).to.equal("Internal error");
-//   });
-// });
+  it("Failed SMS notification due to DB save", async () => {
+    helperMain.__set__("sendSMSNotification", sendSMSNotificationMock);
+    helperMain.__set__("updateDBNotification", updateDBNotificationMockWithException);
+    const resp = await helperMain.smsNotificationHelper({
+      message_content: "kvnhdlvs",
+      mobile: "+91838385415",
+      notification_id: 100,
+    });
+    expect(resp.error).to.equal(true);
+    expect(resp.message).to.equal("Internal error");
+  });
+});
 
 
 
-// helperMain.__set__("notification_audits.update", async () => {
-//   return {
-//     id: 1233,
-//   };
-// });
-// describe("Update Notification test suit ", () => {
-//   it("Successfully updated notification with success response", async () => {
-//     const resp = await helperMain.updateDBNotification(
-//       { error: false, message: "fhvlf" },
-//       79,
-//     );
-//     console.log("hello ", resp);
-//     expect(resp.error).to.equal(false);
-//   });
+helperMain.__set__("notification_audits.update", async () => {
+  return {
+    id: 1233,
+  };
+});
+describe("Update Notification test suit ", () => {
+  it("Successfully updated notification with success response", async () => {
+    const resp = await helperMain.updateDBNotification(
+      { error: false, message: "fhvlf" },
+      79,
+    );
+    console.log("hello ", resp);
+    expect(resp.error).to.equal(false);
+  });
 
-//   it("Successfully updated notification with failed response", async () => {
-//     const resp = await helperMain.updateDBNotification(
-//       { error: true, message: "fhvlf" },
-//       79,
-//     );
-//     console.log("hello ", resp);
-//     expect(resp.error).to.equal(false);
-//   });
+  it("Successfully updated notification with failed response", async () => {
+    const resp = await helperMain.updateDBNotification(
+      { error: true, message: "fhvlf" },
+      79,
+    );
+    console.log("hello ", resp);
+    expect(resp.error).to.equal(false);
+  });
 
-//   it("Invalid params error", async () => {
-//     const resp = await helperMain.updateDBNotification(79);
-//     console.log("hello ", resp);
-//     expect(resp.error).to.equal(true);
-//   });
+  it("Invalid params error", async () => {
+    const resp = await helperMain.updateDBNotification(79);
+    console.log("hello ", resp);
+    expect(resp.error).to.equal(true);
+  });
 
-//   it("notification audit update failed case", async () => {
-//     helperMain.__set__("notification_audits.update", async () => {
-//       throw new Error("updation failed");
-//     });
-//     const resp = await helperMain.updateDBNotification(
-//       { err: false, message: "fhvlf" },
-//       79,
-//     );
-//     console.log("hello ", resp);
-//     expect(resp.error).to.equal(true);
-//   });
-// });
+  it("notification audit update failed case", async () => {
+    helperMain.__set__("notification_audits.update", async () => {
+      throw new Error("updation failed");
+    });
+    const resp = await helperMain.updateDBNotification(
+      { err: false, message: "fhvlf" },
+      79,
+    );
+    console.log("hello ", resp);
+    expect(resp.error).to.equal(true);
+  });
+});
 
-// helperMain.__set__("sendSMS", async () => {
-//   return {
-//     error: false,
-//     message: {},
-//   };
-// });
+helperMain.__set__("sendSMS", async () => {
+  return {
+    error: false,
+    message: {},
+  };
+});
 
-// describe("Send SMS test suit ", () => {
-//   it("Successfully send sms notification", async () => {
-//     const resp = await helperMain.sendSMSNotification(
-//       "kvnhdlvs",
-//       "+91838385415",
-//     );
-//     console.log("hello ", resp);
-//     expect(resp.error).to.equal(false);
-//   });
+describe("Send SMS test suit ", () => {
+  it("Successfully send sms notification", async () => {
+    const resp = await helperMain.sendSMSNotification(
+      "kvnhdlvs",
+      "+91838385415",
+    );
+    console.log("hello ", resp);
+    expect(resp.error).to.equal(false);
+  });
 
-//   it("sns publish error case", async () => {
-//     helperMain.__set__("sendSMS", async () => {
-//       return {
-//         error: true,
-//         message: {},
-//       };
-//     });
+  it("sns publish error case", async () => {
+    helperMain.__set__("sendSMS", async () => {
+      return {
+        error: true,
+        message: {},
+      };
+    });
 
-//     const resp = await helperMain.sendSMSNotification(
-//       "kvnhdlvs",
-//       "+91838385415",
-//     );
-//     console.log("hello ", resp);
-//     expect(resp.error).to.equal(true);
-//   });
-// });
-test('1 + 1 = 2', () => {
-  expect(1 + 1).toBe(2);
+    const resp = await helperMain.sendSMSNotification(
+      "kvnhdlvs",
+      "+91838385415",
+    );
+    console.log("hello ", resp);
+    expect(resp.error).to.equal(true);
+  });
 });
